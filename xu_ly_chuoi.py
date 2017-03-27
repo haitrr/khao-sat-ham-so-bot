bab7580c18420eb2449f7b22a5f24545be388b5f
vroyibg <vroyibg@gmail.com>
2017-03-27 17:48:44 +0700

    Hoàn thiện cấu trúc

--- xu_ly_chuoi.py
+++ xu_ly_chuoi.py
@@ -1,6 +1,7 @@
 import sympy
 from latex2sympy.process_latex import process_sympy
 import re
+import regex
 import hang_so
 
 
@@ -13,17 +14,19 @@ def boc_mpl(bieu_thuc):
 
 
 def tao_latex(bieu_thuc):
+    sympy.init_printing(order='old')
     return sympy.latex(bieu_thuc)
 
 
 def tao_ngoac_nhon(cac_bieu_thuc):
-    if len(cac_bieu_thuc)==1:
+    if len(cac_bieu_thuc) == 1:
         return sympy.latex(cac_bieu_thuc[0])
     ngoac_nhon = "\\begin{cases}"
     for bieu_thuc in cac_bieu_thuc:
         ngoac_nhon += sympy.latex(bieu_thuc) + " \\" + "\\ "
     ngoac_nhon += "\\end{cases}"
     return ngoac_nhon
+
 
 mau_thay = {
     '[àáảãạăắằẵặẳâầấậẫẩ]': 'a',
@@ -46,6 +49,13 @@ def chuyen_thanh_khong_dau_thuong(tin_nhan):
 
 
 def chuyen_latex_thanh_sympy(bieu_thuc):
+    try:
+        return xet_tap_hop(bieu_thuc)
+    except:
+        pass
+    return sympy.sympify(process_sympy(bieu_thuc))
+
+def latex_thanh_sympy(bieu_thuc):
     return sympy.sympify(process_sympy(bieu_thuc))
 
 def tao_anh_html(file_tam):
@@ -55,3 +65,45 @@ def tao_anh_html(file_tam):
     :return: string
     """
     return "<img src=\"{0}\">".format(hang_so.THU_MUC_TAM + file_tam)
+
+
+khop_tap_hop = r'^(?:(?:\s?\\left(\(|\))\s?([^,]+)\,\s?([^,\\]+)\\right(\)|\])\s?(?:(\\cup|\\cap)\s?\\left(\(|\))\s?([^,]+)\,\s?([^,\\]+)\\right(\)|\])\s?)*)|(\s?\\varnothing\s?))$'
+def xet_tap_hop(tin_nhan):
+    khop = regex.match(khop_tap_hop,tin_nhan)
+    if khop:
+        if khop.groups()[-1] is not None:
+            return sympy.EmptySet()
+        else:
+            tap_hop = tao_tap_hop(khop.groups()[0:4])
+            if khop.groups()[4] is not None:
+                toan_tu = khop.captures(5)
+                du_lieu=list(zip(khop.captures(6),khop.captures(7),khop.captures(8),khop.captures(9)))
+                for i in range(len(toan_tu)):
+                    if toan_tu[i]=='\cup':
+                        tap_hop=tap_hop.union(tao_tap_hop(du_lieu[i]))
+                    else:
+                        tap_hop = tap_hop.intersect(tao_tap_hop(du_lieu[i]))
+            return tap_hop
+
+    raise ValueError
+
+def tao_tap_hop(du_lieu):
+    trai = True
+    phai = True
+    bat_dau = None
+    ket_thuc = None
+    if du_lieu[0] == '[':
+        trai = False
+    try:
+        bat_dau = latex_thanh_sympy(du_lieu[1])
+        ket_thuc = latex_thanh_sympy(du_lieu[2])
+    except:
+        raise ValueError
+    if du_lieu[3] == ']':
+        phai = False
+    tap_hop = sympy.Interval(bat_dau, ket_thuc, trai, phai)
+    return tap_hop
+
+if __name__=='__main__':
+    print(xet_tap_hop('\left(2,3\\right)\cup\left(3,4\\right)\cup\left(8,16\\right)'))
+    #print(regex.match(khop_tap_hop,'\left(2,3\\right]\cup\left(3,4\\right)\cup\left(8,16\\right)').captures(8))
