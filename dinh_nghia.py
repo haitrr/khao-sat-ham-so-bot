6c72cdbb5dac6311cf08db83859564fcad54c788
vroyibg <vroyibg@users.noreply.github.com>
2017-06-28 15:25:16 +0700

    Them dinh nghia cuc tri nam o hai phia truc hoanh,tung

--- dinh_nghia.py
+++ dinh_nghia.py
@@ -38,3 +38,23 @@ DE_HAM_SO_DAT_CUC_TRI_TAI_MOT_DIEM = DinhNghia(
         dhk=xu_ly_chuoi.boc_mathjax("f'(k)=0")
     )
 )
+
+DE_HAM_SO_CO_CUC_TRI_NAM_O_HAI_PHIA_TRUC_TUNG = DinhNghia(
+    ten='Điều kiện để hàm số có cực trị nằm ở hai phía trục tung',
+    noi_dung = 'Để hàm số {fx} có cực trị nằm ở hai phía trục tung thì {fx} đạt cực trị tại điểm {x1} và {x2} sao cho {dk}'.format(
+        fx= xu_ly_chuoi.boc_mathjax('f(x)'),
+        x1=xu_ly_chuoi.boc_mathjax('x_1'),
+        x2=xu_ly_chuoi.boc_mathjax('x_2'),
+        dk=xu_ly_chuoi.boc_mathjax('x_1 x_2<0')
+    )
+)
+
+DE_HAM_SO_CO_CUC_TRI_NAM_O_HAI_PHIA_TRUC_HOANH = DinhNghia(
+    ten='Điều kiện để hàm số có cực trị nằm ở hai phía trục hoành',
+    noi_dung = 'Để hàm số {fx} có cực trị nằm ở hai phía trục hoành thì {fx} đạt cực trị tại điểm {x1} và {x2} sao cho {dk}'.format(
+        fx= xu_ly_chuoi.boc_mathjax('f(x)'),
+        x1=xu_ly_chuoi.boc_mathjax('(x_1,y_1)'),
+        x2=xu_ly_chuoi.boc_mathjax('(x_2,y_2)'),
+        dk=xu_ly_chuoi.boc_mathjax('y_1 y_2<0')
+    )
+)
